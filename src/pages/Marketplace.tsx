2adadc1139389d97f93bab5454fea57ad3c7a5d5
Samuel Edu <edubsam@gmail.com>
2026-03-17 16:33:43 +0100

    fix: add missing Link import in Marketplace.tsx

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,4 +1,5 @@
 import { useState, useMemo } from "react";
+import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
